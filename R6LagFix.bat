f925c64993995a3bceb843c6e15f278e2a4fbfbb
jackdaniels552 <rwbessette2015@gmail.com>
2023-02-05 16:16:32 -0500

    Automatic
    Script should automatically adjust for your CPU. Make sure the core count is correct when it is displayed at the start.

--- R6LagFix.bat
+++ R6LagFix.bat
@@ -1,75 +1,45 @@
 @echo off
-color 0a
 
-echo --------------Rainbow Six Siege Lag fix----------------
-echo.
-echo -------------Version 1.2--------------
-echo.
-echo -------------By Jack Daniels--------------
-echo.
+:: Created by Jack Daniels
+:: Version 1.3 Pre-Release
 
-:confirm
-echo -------------Have you edited the processor value in this script to match your CPU?--------------
-echo.
-echo -------------If not, edit now and run the script again. If yes, press any key to continue--------------
-pause >nul
+echo Author: Jack Daniels
+echo Version: 1.3 Pre-Release
+echo WARNING: Use of this script is at your own risk.
+echo Please report any issues on the GitHub page: https://github.com/jackdaniels552/r6lagfix.
 
-:choose
-echo.
-echo --------------Choose an option--------------
-echo.
-echo --------------1. Manual - RainbowSix_Vulkan--------------
-echo --------------2. Manual - RainbowSix--------------
-echo --------------3. Automatic--------------
-echo.
+for /f %%i in ('powershell [System.Environment]::ProcessorCount') do set numProcessors=%%i
+echo Number of processors on the system: %numProcessors%
+set /p confirm=Please confirm (yes/no): 
 
-set /p option=Enter option number:
-
-:: Option 1 - Manual with RainbowSix_Vulkan process
-if %option%==1 (
-:loop1
-timeout /t 600
-PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=8"
-timeout /t 1
-PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=15"
-goto loop1
-)
-
-:: Option 2 - Manual with RainbowSix process
-if %option%==2 (
-:loop2
-timeout /t 600
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=8"
-timeout /t 1
-PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=15"
-goto loop2
-)
-
-:: Option 3 - Automatic
-if %option%==3 (
-  :check_process
-  if exist "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" (
-    :process_vulkan
-    PowerShell "$Process = Get-Process RainbowSix_Vulkan; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15}"
-    goto process_rainbow
-  ) else (
-    echo PowerShell not found. Exiting...
-    pause
-    exit
-  )
-  :process_rainbow
-  PowerShell "$Process = Get-Process RainbowSix; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15}"
-  
-  :ask_continue
-  echo Do you want to continue checking for both processes? (Y/N)
-  set /p answer=
-  if /I %answer% NEQ Y (
-    exit
-  ) else (
-    goto check_process
+if /i "%confirm%" NEQ "yes" (
+  if /i "%confirm%" NEQ "y" (
+    echo Please report this on the GitHub page: https://github.com/jackdaniels552/r6lagfix
+    goto end
   )
 )
 
-:: Invalid option entered
-echo --------------Invalid option entered. Restarting script--------------
-goto confirm
+:loop
+echo Running the script...
+
+powershell -Command "$rainbowSix = Get-Process | Where-Object { $_.ProcessName -eq 'RainbowSix' -or $_.ProcessName -eq 'RainbowSix_Vulkan' }; if ($rainbowSix) { foreach ($process in $rainbowSix) { $process.ProcessorAffinity = [Math]::Pow(2, %numProcessors% - 1) - 1 } } else { Write-Host 'Neither RainbowSix nor RainbowSix_Vulkan processes were detected.'; break }"
+
+timeout /t 600
+
+set /p stop=Do you want to stop the script? (yes/no): 
+if /i "%stop%" == "yes" (
+  if /i "%stop%" == "y" (
+    echo Stopping the script...
+    goto end
+  )
+) else (
+  if /i "%stop%" == "no" (
+    if /i "%stop%" == "n" (
+      goto loop
+    )
+  )
+)
+
+:end
+echo The script has been stopped.
+pause
