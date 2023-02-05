20c4c01d229661a27fb7b0f1d42b2f06d4dac898
jackdaniels552 <rwbessette2015@gmail.com>
2023-02-05 14:23:47 -0500

    Automatic option fix
    Automatic option to check for both processes fixed.

--- R6LagFix.bat
+++ R6LagFix.bat
@@ -3,7 +3,7 @@ color 0a
 
 echo --------------Rainbow Six Siege Lag fix----------------
 echo.
-echo -------------Version 1.0--------------
+echo -------------Version 1.2--------------
 echo.
 echo -------------By Jack Daniels--------------
 echo.
@@ -23,37 +23,53 @@ echo --------------2. Manual - RainbowSix--------------
 echo --------------3. Automatic--------------
 echo.
 
-set /p option=Enter option number: 
+set /p option=Enter option number:
 
+:: Option 1 - Manual with RainbowSix_Vulkan process
 if %option%==1 (
-  :loop1
-  timeout /t 600
-  PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=8"
-  timeout /t 1
-  PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=15"
-  goto loop1
+:loop1
+timeout /t 600
+PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=8"
+timeout /t 1
+PowerShell "$Process = Get-Process RainbowSix_Vulkan; $Process.ProcessorAffinity=15"
+goto loop1
 )
 
+:: Option 2 - Manual with RainbowSix process
 if %option%==2 (
-  :loop2
-  timeout /t 600
-  PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=8"
-  timeout /t 1
-  PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=15"
-  goto loop2
+:loop2
+timeout /t 600
+PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=8"
+timeout /t 1
+PowerShell "$Process = Get-Process RainbowSix; $Process.ProcessorAffinity=15"
+goto loop2
 )
 
+:: Option 3 - Automatic
 if %option%==3 (
   :check_process
   if exist "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" (
-    PowerShell "$Process = Get-Process RainbowSix_Vulkan; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15; goto check_process}"
-    PowerShell "$Process = Get-Process RainbowSix; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15; goto check_process}"
+    :process_vulkan
+    PowerShell "$Process = Get-Process RainbowSix_Vulkan; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15}"
+    goto process_rainbow
   ) else (
     echo PowerShell not found. Exiting...
     pause
     exit
   )
+  :process_rainbow
+  PowerShell "$Process = Get-Process RainbowSix; if ($Process -ne $null) {$Process.ProcessorAffinity=8; Start-Sleep -s 1; $Process.ProcessorAffinity=15}"
+  
+  :ask_continue
+  echo Do you want to continue checking for both processes? (Y/N)
+  set /p answer=
+  if /I %answer% NEQ Y (
+    exit
+  ) else (
+    goto check_process
+  )
 )
 
+:: Invalid option entered
 echo --------------Invalid option entered. Restarting script--------------
 goto confirm
